a35a5fd8dfab5b99c5a769236b5b577af5734799
mignonnesaurus <elifloresch@gmail.com>
2017-08-26 18:50:54 +0200

    Commenting for Rails Girls App

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170805185940) do
+ActiveRecord::Schema.define(version: 20170826164403) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "user_name"
+    t.text "body"
+    t.integer "idea_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "ideas", force: :cascade do |t|
     t.string "name"
