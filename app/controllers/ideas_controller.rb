51c305880b6aa6a6597510b00056a7fa1c16caf4
mignonnesaurus <elifloresch@gmail.com>
2017-08-26 17:57:42 +0200

    Some updates :D

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -61,14 +61,18 @@ class IdeasController < ApplicationController
     end
   end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_idea
-      @idea = Idea.find(params[:id])
-    end
+  def idea_path(idea)
+    ideas_path + '/' + idea.id.to_s
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def idea_params
-      params.require(:idea).permit(:name, :description, :picture)
-    end
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_idea
+    @idea = Idea.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def idea_params
+    params.require(:idea).permit(:name, :description, :picture)
+  end
 end
