7e4d59795babf0cf00b331388307253871744612
mignonnesaurus <elifloresch@gmail.com>
2017-08-30 12:23:30 +0200

    Add name custom field :)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170830091054) do
+ActiveRecord::Schema.define(version: 20170830095331) do
 
   create_table "comments", force: :cascade do |t|
     t.string "user_name"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20170830091054) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
